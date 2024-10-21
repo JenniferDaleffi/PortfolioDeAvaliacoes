38c57c400729dd0726a39e73a5792e2c8f537cd1
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-10-21 19:09:12 -0300

    apagando icons antigos para outros icons no Menu.tsx

--- portfolio/src/components/Menu/menu.tsx
+++ portfolio/src/components/Menu/menu.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 import Link from "next/link"
-import book from "../../../public/images/book-open.png"
-import earth from "../../../public/images/earth.png"
-import target from "../../../public/images/target.png"
 import checkpoint from "../../../public/images/checkpoint.png"
+import global from "../../../public/images/global.png"
+import challenge from "../../../public/images/challenge.png"
+
 export default function Menu() {
     return (
         <div>
@@ -15,13 +15,13 @@ export default function Menu() {
                     <Link href="/checkpoint" className="px-4 py-2 bg-fiap rounded-md text-white"> Checkpoint </Link>
                 </div>
                 <div className="flex flex-col items-center w-1/3 m-10 min-h-80 shadow-lg">
-                    <Image src={target} alt="challenge" width={50} height={50} className="my-4" />
+                    <Image src={challenge} alt="challenge" width={100} height={50} className="my-4" />
                     <h2 className="text-2xl mb-4">Checkpoint</h2>
                     <p className="text-center mb-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, aliquid nobis? Non dolorem, in ad laborum adipisci ipsa modi provident!</p>
                     <Link href="/checkpoint" className="px-4 py-2 bg-fiap rounded-md text-white"> Checkpoint </Link>
                 </div>
                 <div className="flex flex-col items-center w-1/3 m-10 min-h-80 shadow-lg">
-                    <Image src={earth} alt="Global Sollution" width={50} height={50} className="my-4" />
+                    <Image src={global} alt="Global Sollution" width={100} height={50} className="my-4" />
                     <h2 className="text-2xl mb-4">Checkpoint</h2>
                     <p className="text-center mb-12">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, aliquid nobis? Non dolorem, in ad laborum adipisci ipsa modi provident!</p>
                     <Link href="/checkpoint" className="px-4 py-2 bg-fiap rounded-md text-white"> Checkpoint </Link>

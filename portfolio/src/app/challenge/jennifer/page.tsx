7f33595f5cf3cef8d509dc93bd272c4a30d02de3
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-10-27 01:09:48 -0300

    subindo as paginas estilizadas do challenge

--- portfolio/src/app/challenge/jennifer/page.tsx
+++ portfolio/src/app/challenge/jennifer/page.tsx
@@ -4,8 +4,10 @@ import iconJava from "../../../../public/images/iconjava.png"
 import iconPy from "../../../../public/images/icons8-python-100.png"
 import Jenniferfoto from "../../../../public/images/jennifer.png"
 import iconMongo from "../../../../public/images/mongodb.png"
-
-
+import JenniChallenge from "../../../../public/images/challengejenni.png"
+import CalendarioChallenge from "../../../../public/images/calendariochallenge.png"
+ 
+ 
 export default function Jennifer() {
     return (
         <div className="bg-black">
@@ -31,6 +33,7 @@ export default function Jennifer() {
                     <Image className="rounded-full w-80 h-80 " src={Jenniferfoto} alt="Foto da Jennifer" />
                     <h1 className="text-2xl mt-5 text-fiap" >Back-end Java e</h1>
                     <h1 className="text-2xl mt-3 text-fiap" >Análise de Dados</h1>
+                    <Image className="mt-20" width={350} src={CalendarioChallenge} alt="imagem do calendario challenge"></Image>
                 </div>
                 <div className="max-w-xl">
                     <h1 className="text-5xl text-fiap" >Jennifer Daleffi</h1>
@@ -39,6 +42,33 @@ export default function Jennifer() {
                         <Image width={90} src={iconPy} alt="python"></Image>
                         <Image width={90} src={iconJava} alt="java"></Image>
                         <Image width={90} src={iconMongo} alt="mongo"></Image>
+                    </div>
+                    <div className="mt-10">
+                        <Image className="rounded-lg" src={JenniChallenge} alt="imagem do FixerAI"></Image>
+                        <h1 className="mt-5 text-fiap text-xl">FixerAI</h1>
+                        <p className="text-lg text-white">O projeto "ReciclaBalsa: Transporte
+                        Marítimo Sustentável para a Região de São Paulo" propõe de forma multidisciplinar contendo uma base de dados, python, java, frontend, business model e inteligência artificial a criação que facilite a integração de iniciativas de reciclagem com o transporte fluvial, visando promover práticas sustentáveis e reduzir a emissão de poluentes.</p>
+                    </div>
+                    <div className="mt-20 border-2 border-white border-solid rounded-lg shadow-2xl shadow-fiap hover:scale-110 hover:duration-300 mb-20">
+                        <h1 className="text-white p-10 text-3xl">Feedback <br /> <span className="text-fiap">Challenge</span> </h1>
+                        <h1 className="text-fiap ml-10 text-2xl">Artificial Inteligence:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">
+                        Através do video ficou claro o uso do Watson com Telegram e o reconhecimento das inteções, entidades e variaveis!</span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Building Relational Database:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">
+                        Os relacionamentos entre as entidades CLIENTES, TELEFONE, ENDERECO e OFICINAS está definido como 1:1;</span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Computational Thinking Using Python:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">Formato de entrega: 0,5. Documentação - 2,5 ok. Código fonte - 5,5. Melhorar a apresentação dos dados e usabilidade
+                        </span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Domain Driven Using Java:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">Efetuados todos os requisitos de avaliação, parabéns a todos envolvidos pelo excelente trabalho.
+                        </span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Front End Design Engineering:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">Versionamento: Não foi enviado uma cópia do repositório e sim apenas uma pasta com arquivos.(-10)
+                        Estrutura: Muitos links internos abrindo em outra aba..
+                        </span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Software Engineering and Business Model:</h1>
+                        <p className="text-white ml-10 mb-5"><span className="text-amarelo">Faltou a Descrição dos casos de uso dentro dos Diagramas de caso de uso. No canvas. Pensar em colocar ao projetar os custos e as receitas estimativas. Revisão de texto!</span></p>
                     </div>
                 </div>
                 <div className="mr-10">
@@ -54,45 +84,45 @@ export default function Jennifer() {
                                 <tr className="border-solid border-2 border-black">
                                     <th scope="col">DISCIPLINA</th>
                                     <th scope="col">SPNT</th>
-
+ 
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Artificial Intelligence</th>
-
+ 
                                     <td className="p-5">100</td>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Building Relational Database</th>
-
+ 
                                     <td className="p-5">75</td>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Computational Thinking Using Python</th>
-
+ 
                                     <td className="p-5">75</td>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Domain Driven Using Java</th>
-
+ 
                                     <td className="p-5">90</td>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Front End Design Engineering</th>
-
+ 
                                     <td className="p-5">35</td>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Software Engineering and Business Model</th>
-
+ 
                                     <td className="p-5">80</td>
-
+ 
                                 </tr>
                             </tbody>
                         </table>
@@ -106,19 +136,19 @@ export default function Jennifer() {
                                 <tr className="border-solid border-2 border-black">
                                     <th scope="col">DISCIPLINA</th>
                                     <th scope="col">SPNT</th>
-
+ 
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Artificial Intelligence</th>
                                     <th className="p-5">100</th>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Building Relational Database</th>
                                     <th className="p-5">91</th>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Computational Thinking Using Python</th>
@@ -135,7 +165,7 @@ export default function Jennifer() {
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Software Engineering and Business Model</th>
                                     <th className="p-5">75</th>
-                                    
+                                   
                                 </tr>
                             </tbody>
                         </table>
@@ -143,6 +173,7 @@ export default function Jennifer() {
                 </div>
             </div>
         </div>
-
+ 
     )
 }
+ 
--- portfolio/src/app/challenge/julia/page.tsx
+++ portfolio/src/app/challenge/julia/page.tsx
@@ -4,9 +4,9 @@ import iconJava from "../../../../public/images/iconjava.png"
 import iconPy from "../../../../public/images/icons8-python-100.png"
 import Juliafoto from "../../../../public/images/juliavasco.jpg"
 import iconType from "../../../../public/images/type.png"
-
-
-
+import LeoJuChallenge from "../../../../public/images/challengeleoju.jpg"
+import CalendarioChallenge from "../../../../public/images/calendariochallenge.png"
+ 
 export default function Julia() {
     return (
         <div className="bg-black">
@@ -31,6 +31,7 @@ export default function Julia() {
                 <div className="text-center">
                     <Image className="rounded-full w-80 h-80 " src={Juliafoto} alt="Foto da Julia" />
                     <h1 className="text-2xl mt-5 text-fiap" >Analista de Sistemas</h1>
+                    <Image className="mt-20" width={350} src={CalendarioChallenge} alt="imagem do calendario challenge"></Image>
                 </div>
                 <div className="max-w-xl">
                     <h1 className="text-5xl text-fiap" >Julia Vasconcelos</h1>
@@ -39,6 +40,33 @@ export default function Julia() {
                         <Image width={90} src={iconPy} alt="python"></Image>
                         <Image width={90} src={iconJava} alt="java"></Image>
                         <Image width={90} src={iconType} alt="type"></Image>
+                    </div>
+                    <div className="mt-10">
+                        <Image className="rounded-lg" src={LeoJuChallenge} alt="challenge leo e julia"></Image>
+                        <h1 className="mt-5 text-xl text-fiap">ConectCar</h1>
+                        <p className="text-lg break-all text-white">O projeto "ConectCar" apresenta uma solução inovadora para o mercado de reparação automotiva, enfocando a experiência digital e a eficiência dos serviços prestados. A proposta central é a criação de um sistema integrado que permite aos clientes do Centro Automotivo Porto a realização de diagnósticos veiculares e a contratação de serviços de reparo de forma online, sem a necessidade de deslocamento até a oficina mecânica para a primeira avaliação.</p>
+                    </div>
+                    <div className="mt-20 border-2 border-white border-solid rounded-lg shadow-2xl shadow-fiap hover:scale-110 hover:duration-300 mb-20">
+                        <h1 className="text-white p-10 text-3xl">Feedback <br /> <span className="text-fiap">Challenge</span> </h1>
+                        <h1 className="text-fiap ml-10 text-2xl">Artificial Inteligence:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">
+                        No geral, o projeto ConnectCar apresenta uma solução bem estruturada para modernizar o setor de serviços automotivos.</span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Building Relational Database:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">
+                        Não entregou o descritivo do projeto (arquivo docx vazio);</span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Computational Thinking Using Python:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">Ok - conceitos aplicados na solução, porém, poderiam ter explorado mais o que tange a regra de negócio da solução proposta.
+ 
+                        </span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Domain Driven Using Java:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">No Diagrama de classes (UML) é plausível uma maior abstração dos atributos nas classes Diagrama de classes para resolução do problema proposto
+ 
+                        </span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Front End Design Engineering:</h1>
+                        <p className="text-white ml-10"><span className="text-amarelo">O projeto ficou bom, apesar das falhas técnicas.
+                        </span></p>
+                        <h1 className=" mt-3 text-fiap ml-10 text-2xl">Software Engineering and Business Model:</h1>
+                        <p className="text-white ml-10 mb-5"><span className="text-amarelo">Ficou legal todos os membros do grupo participarem do vídeo.</span></p>
                     </div>
                 </div>
                 <div className="mr-10">
@@ -54,45 +82,32 @@ export default function Julia() {
                                 <tr className="border-solid border-2 border-black">
                                     <th scope="col">DISCIPLINA</th>
                                     <th scope="col">SPNT</th>
-                                    <th scope="col">FEEDBACK</th>
-
                                 </tr>
                             </thead>
                             <tbody>
                             <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Artificial Intelligence</th>
-                                    <td>100</td>
-                                    <th>No geral, o projeto ConnectCar apresenta uma solução bem estruturada para modernizar o setor de serviços automotivos.</th>
-
+                                    <td className="p-5">100</td>
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Building Relational Database</th>
-                                    <th>65</th>
-                                    <td className="p-5">Não entregou o descritivo do projeto (arquivo docx vazio);</td>
+                                    <th className="p-5">65</th>
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Computational Thinking Using Python</th>
-                                    <th>74</th>
-                                    <td className="p-5">ok - conceitos aplicados na solução, porém, poderiam ter explorado mais o que tange a regra de negócio da solução proposta.
-
-                                    </td>
-                                  
+                                    <th className="p-5">74</th>        
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Domain Driven Using Java</th>
-                                    <th>96</th>
-                                    <td className="p-5">no Diagrama de classes (UML) é plausível uma maior abstração dos atributos nas classes Diagrama de classes para resolução do problema proposto</td>
+                                    <th className="p-5">96</th>
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Front End Design Engineering</th>
-                                    <th>85</th>
-                                    <td className="p-5">O projeto ficou bom, apesar das falhas técnicas.</td>
+                                    <th className="p-5">85</th>
                                     </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Software Engineering and Business Model</th>
-                                    <th>77</th>
-                                    <td className="p-5">Ficou legal todos os membros do grupo participarem do vídeo. </td>
-                                    
+                                    <th className="p-5">77</th>                    
                                 </tr>
                             </tbody>
                         </table>
@@ -106,19 +121,19 @@ export default function Julia() {
                                 <tr className="border-solid border-2 border-black">
                                     <th scope="col">DISCIPLINA</th>
                                     <th scope="col">SPNT</th>
-
+ 
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Artificial Intelligence</th>
                                     <th className="p-5">--</th>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Building Relational Database</th>
                                     <th className="p-5">--</th>
-
+ 
                                 </tr>
                                 <tr className="border-solid border-2 border-black">
                                     <th className="border-solid border-2 border-black">Computational Thinking Using Python</th>
@@ -142,6 +157,6 @@ export default function Julia() {
                 </div>
             </div>
         </div>
-
+ 
     )
 }

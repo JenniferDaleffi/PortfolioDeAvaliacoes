f93213c39d2a96a06c35f2f0a66f7f71444e273f
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-10-27 18:15:00 -0300

    arrumando o menu com os dashboards

--- portfolio/src/components/Cabecalho/cabecalho.tsx
+++ portfolio/src/components/Cabecalho/cabecalho.tsx
@@ -3,6 +3,10 @@ import Nav from "../Nav/nav";
 import Image from "next/image";
 import Jennifer from "../../../public/images/jennifer.png"
 import grafico from "../../../public/images/Blue and White Weekly House Expenses Chart Graph.png"
+import dashboardJenni from "../../../public/images/dashboardjenni.png"
+import dashboardLeo from "../../../public/images/dashboardleo.png"
+import dashboardJulia from "../../../public/images/dashboardjulia.png"
+import Book from "../../../public/images/book.png"
  
 export default function Cabecalho() {
     return (
@@ -10,11 +14,23 @@ export default function Cabecalho() {
             <header>
                 <Nav></Nav>
                 <div className="flex justify-around">
-                    <div className="text-left">
-                        <h1 className="text-5xl mt-20">Portfólio <span className="text-fiap">dos Estudantes</span></h1>
-                       
+                    <div className="text-left max-w-80">
+                        <h1 className="text-5xl mt-20">Portfólio</h1>
+                        <span className="text-fiap text-5xl">dos Estudantes</span>
+                        <p className="break-all mt-10">Esse portfólio é composto por três estudantes da Faculdade FIAP do curso de Análise e Desenvolvimento de Sistemas e tem como intuito criar um site compartilhado sobre checkpoints, challenges e as globals solutions. De forma organizada os estudantes podem consultar suas médias gerais do 1º Semestre e 2º Semestre, uma maneira de observação com desempenho geral.</p>
+                        <Image className="mt-10" width={300} src={Book} alt="imagem de um livro"></Image>
+                    </div>
+                    <div className="bg-black flex flex-col items-center gap-10">
+                        <div className="flex justify-around mt-20 mb-10 bg-black gap-10">
+                            <Image className="rounded-lg border-white border-2 border-solid p-5 hover:scale-110 hover:duration-300" width={500} src={dashboardJenni} alt="dashboard da jennifer"></Image>
+                            <Image className="rounded-lg border-white border-2 border-solid p-5 hover:scale-110 hover:duration-300" width={500} src={dashboardLeo} alt="dashboard do leonardo"></Image>
+                        </div>
+                        <div>
+                            <Image className="rounded-lg border-white border-2 border-solid p-5 hover:scale-110 hover:duration-300" width={500} src={dashboardJulia} alt="dashboard da julia"></Image>
+                        </div>
                     </div>
                 </div>
+               
             </header>
             <Menu />
         </div>
--- portfolio/src/components/Menu/menu.tsx
+++ portfolio/src/components/Menu/menu.tsx
@@ -3,98 +3,37 @@ import challenge from "../../../public/images/challenge.svg"
 import checkpoint from "../../../public/images/checkpoint.svg"
 import global from "../../../public/images/global.svg"
 import Link from "next/link"
+import dashboardJenni from "../../../public/images/dashboardjenni.png"
+import dashboardLeo from "../../../public/images/dashboardleo.png"
+import dashboardJulia from "../../../public/images/dashboardjulia.png"
  
 export default function Menu() {
     return (
-        <div>
-            <div className="flex items-center justify-center w-full h-screen">
+        <div className="bg-black">
+            <div className="flex items-center justify-center">
                 <div className="p-10 flex flex-col items-center justify-center w-1/3 m-10 h-2/3 border-white border-solid border-2 shadow-slate-300 shadow-lg rounded-lg hover:scale-110 hover:duration-300">
                     <Image src={checkpoint} alt="Checkpoint" width={100} height={50} className="my-4" />
                     <h2 className="text-2xl mb-4">CheckPoint</h2>
                     <p className="text-left mb-12"> São testes semestrais dados por cada matéria do curso de Análise e Desenvolvimento de Sistemas, Sendo: Building relational Database, Domain driven using java, Computational thinking using python, IA, Front-end design engineering e Software engineering and business model.</p>
-                    <Link className="text-white border-black border-solid border-2 p-3 bg-fiap rounded-md hover:bg-black hover:border-white  transition duration-500 hover:scale-110 hover:duration-300" href="/us/jennifer">CheckPoints</Link>
+                    <Link className="text-white border-black border-solid border-2 p-3 bg-fiap rounded-md hover:bg-black hover:border-white  transition duration-500 hover:scale-110 hover:duration-300" href="/checkpoint/jennifer">CheckPoints</Link>
                 </div>
                 <div className="p-10 flex flex-col items-center justify-center w-1/3 m-10 h-2/3 border-white border-solid border-2 shadow-slate-300 shadow-lg rounded-lg hover:scale-110 hover:duration-300">
                     <Image src={global} alt="challenge" width={100} height={50} className="my-4" />
                     <h2 className="text-2xl mb-4">Global Solution</h2>
                     <p className="text-left mb-12">A Global Solution da FIAP tem como objetivo resolver problemas de situacões reais e globais em parcerias com várias empresas do Mundo todo! Estudantes da FIAP recebem tempo determinado para solucionar casos importantes que contém inovacão como forma de sustentabilidade.
                     </p>
-                    <Link className="text-white border-black border-solid border-2 p-3 bg-fiap rounded-md hover:bg-black hover:border-white  transition duration-500 hover:scale-110 hover:duration-300" href="">Global Solution</Link>
+                    <Link className="text-white border-black border-solid border-2 p-3 bg-fiap rounded-md hover:bg-black hover:border-white  transition duration-500 hover:scale-110 hover:duration-300" href="/gs/jennifer">Global Solution</Link>
                 </div>
                 <div className="p-10 flex flex-col items-center justify-center w-1/3 m-10 h-2/3 border-white border-solid border-2 shadow-slate-300 shadow-lg rounded-lg hover:scale-110 hover:duration-300">
                     <Image src={challenge} alt="Global Sollution" width={100} height={50} className="my-4" />
                     <h2 className="text-2xl mb-4">Challenge</h2>
                     <p className="text-left mb-12">Os challenges da FIAP sao projetos desenvolvidos no ano todo para solucionar uma questão dada por determinada empresa, Os estudantes de Análise e Desenvolvimento de Sistemas estão buscando a inovacão  como forma de solução para porto seguro e a confiabilidade dos clientes em razão a oficinas mecânicas.</p>
-                    <Link className="text-white border-black border-solid border-2 p-3 bg-fiap rounded-md hover:bg-black hover:border-white  transition duration-500 hover:scale-110 hover:duration-300" href="">Challenge</Link>
+                    <Link className="text-white border-black border-solid border-2 p-3 bg-fiap rounded-md hover:bg-black hover:border-white  transition duration-500 hover:scale-110 hover:duration-300" href="/challenge/jennifer">Challenge</Link>
                 </div>
             </div>
-            <div>
-                        <div className=" flex items-center justify-center">
-                            <h1 className="text-4xl text-white" > Desempenho </h1>
-                        </div>
-                        <table className="text-white bg-fiap rounded-lg mb-5 mt-5 justify-content">
-                            <thead>
-                                <tr className="border-solid border-2 border-black">
-                                    <th scope="col">DISCIPLINA</th>
-                                    <th scope="col">Jennifer</th>
-                                    <th scope="col">Julia</th>
-                                    <th scope="col">Leonardo</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr className="border-solid border-2 border-black">
-                                    <th className="border-solid border-2 border-black">Artificial Intelligence</th>
-                                    <th>100</th>
-                                    <td className="p-5">100</td>
-                                    <td>100</td>
-                                    
-                                </tr>
-                                <tr className="border-solid border-2 border-black">
-                                    <th className="border-solid border-2 border-black">Building Relational Database</th>
-                                    <th>90</th>
-                                    <td className="p-5">86</td>
-                                    <td>83</td>
-                                    
-                                </tr>
-                                <tr className="border-solid border-2 border-black">
-                                    <th className="border-solid border-2 border-black">Computational Thinking Using Python</th>
-                                    <th>67</th>
-                                    <td className="p-5">66</td>
-                                    <td>67</td>
-                                    
-                                </tr>
-                                <tr className="border-solid border-2 border-black">
-                                    <th className="border-solid border-2 border-black">Domain Driven Using Java</th>
-                                    <th>97</th>
-                                    <td className="p-5">95</td>
-                                    <td>97</td>
-                                    
-                                </tr>
-                                <tr className="border-solid border-2 border-black">
-                                    <th className="border-solid border-2 border-black">Front End Design Engineering</th>
-                                    <th>81</th>
-                                    <td className="p-5">71</td>
-                                    <td>73</td>
-                                    
-                                </tr>
-                                <tr className="border-solid border-2 border-black">
-                                    <th className="border-solid border-2 border-black">Software Engineering and Business Model</th>
-                                    <th>69</th>
-                                    <td className="p-5">73</td>
-                                    <td>76</td>
-                                </tr>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td colSpan={4}>
-                                        <h2 className="text-center">Quantidade de registros: 6</h2>
-                                    </td>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
         </div>
        
     )
  
 }
+ 

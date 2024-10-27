9b00b7cf13b4d807cc6c9113dbe77de868783603
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-10-27 18:15:49 -0300

    imagem not found

--- portfolio/src/app/not-found.tsx
+++ portfolio/src/app/not-found.tsx
@@ -1,19 +1,13 @@
 import Image from "next/image"
-import errorImage from "../../public/images/not-found.jpg"
+import errorImage from "../../public/images/errorimage.png"
 import Link from "next/link"
-
+ 
 export default function notError() {
     return (
-        <section className="flex flex-col items-center justify-center mt-12 text-neutral-50">
-            <h2 className="text-3xl m-4">OPS! error 404! Page not Found</h2>
-            <figure>
-                <Image src={errorImage}
-                    width={900}
-                    height={900}
-                    alt={"error"} />
-                <figcaption className="text-center text-2xl">Erro 404</figcaption>
-            </figure>
-            <Link href={"/"} className="text-3xl m-16">Voltar para Pagina principal</Link>
+        <section className="flex flex-col items-center justify-center text-neutral-50 bg-black h-screen">
+            <h2 className="text-3xl m-4 text-fiap">OPS! error 404! Page not Found.</h2>
+            <Image width={300} src={errorImage} alt="imagem de erro"></Image>
+            <Link href={"/"} className="text-3xl m-16 border-2 border-solid border-white p-3 rounded-md hover:bg-fiap transition duration-500 hover:border-black">Voltar para Pagina principal</Link>
         </section>
     )
 }
